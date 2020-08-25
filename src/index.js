ee7d316446ed7ebe60b0b5d5f2aaa3a6ace7f09f
blessingmwalein <blessingmwalein@outlook.com>
2020-08-25 12:05:54 +0200

    Add eslint and watching

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import numeral from 'numeral'
 import './index.css'
 
 const courseValue = numeral(1000).format('$0,0.00')
+debugger;
 console.log(`I would pay ${courseValue} from this awesomecourse!`)
